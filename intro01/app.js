9cc71d6444295d5b43491af2a403a42b3ec2910f
itshak6543 <iecga16543@gmail.com>
2025-04-06 14:58:29 +0300

    sasas

---
+++ intro01/app.js
@@ -0,0 +1,12 @@
+//output
+console.log("hello world")
+//var x= 5 , let x = 5 , const x = 5,
+let x = 5;
+console.log(x)
+let name = ("itshak")//string
+let age  = (6.889)
+console.log(name + " is " + age + " years old ")//print 
+let x1 = Math.random() * 100//random double 
+console.log(x1)
+let x2 = Math.floor(Math.random() * 100)// round num
+console.log(x2)
